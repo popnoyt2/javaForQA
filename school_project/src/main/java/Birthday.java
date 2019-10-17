4dfd0b537c2176a86becb6161676a28495e69153
Elina Markova <Elina_Markova@rcomext.com>
2019-10-17 15:09:58 +0300

    setup

--- school_project/src/main/java/Birthday.java
+++
@@ -1,8 +0,0 @@
-public class Birthday {
-
-    public String isItMyBirthday(int month, int day) {
-        if (month == 9 && day == 27)
-            return "Yes,Your B-day is on " + day + "." + month;
-        else return "No, its not your B-day";
-    }
-}
